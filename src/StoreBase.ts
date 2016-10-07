microsoft/ReSub

commit 3999239d3c0c5ef254b764712615f477d5f4186f

0.0.6: Added support for using number types as keys

src/StoreBase.ts
@@ -72,8 +72,8 @@ export abstract class StoreBase {
 
     // If you trigger a specific set of keys, then it will only trigger that specific set of callbacks (and subscriptions marked
     // as "All" keyed).  If the key is all, it will trigger all callbacks.
-    protected trigger(keyOrKeys?: string|string[]) {
-        const keys = _.isString(keyOrKeys) ? [keyOrKeys] : keyOrKeys;
+    protected trigger(keyOrKeys?: string|number|(string|number)[]) {
+        const keys = _.map(_.isArray(keyOrKeys) ? keyOrKeys : [keyOrKeys], (key, i) => _.isNumber(key) ? key.toString() : key);
 
         // Build a list of callbacks to call, trying to accumulate keys into a single callback set to avoid multiple callbacks
         // to the same target with different keys.

src/AutoSubscriptions.ts
@@ -222,11 +222,15 @@ function makeAutoSubscribeDecorator<T extends Function>(shallow = false, default
             const metaForMethod = target.__metadata[methodName];
             assert.ok(metaForMethod, 'Internal failure: what happened to the metadata for this method?');
             if (metaForMethod.hasIndex) {
-                const keyArg = args[metaForMethod.index];
+                let keyArg = args[metaForMethod.index];
 
-                assert.ok(keyArg, '@key parameter must be given a non-empty string: "' + methodName + '"@' + metaForMethod.index
+                if (_.isNumber(keyArg)) {
+                    keyArg = keyArg.toString();
+                }
+
+                assert.ok(keyArg, '@key parameter must be given a non-empty string or number: "' + methodName + '"@' + metaForMethod.index
                     + ' was given ' + JSON.stringify(keyArg));
-                assert.ok(_.isString(keyArg), '@key parameter must be given a string: "' + methodName + '"@' + metaForMethod.index);
+                assert.ok(_.isString(keyArg), '@key parameter must be given a string or number: "' + methodName + '"@' + metaForMethod.index);
 
                 specificKeyValue = keyArg;
             }

src/ComponentBase.ts
@@ -240,7 +240,7 @@ abstract class ComponentBase<P extends React.Props<any>, S extends Object> exten
         return removed;
     }
 
-    private _registerSubscription(subscription: StoreSubscriptionInternal<S>, key = StoreBase.Key_All) {
+    private _registerSubscription(subscription: StoreSubscriptionInternal<S>, key: string|number = StoreBase.Key_All) {
         assert.ok(!subscription._subscriptionToken,
             'Subscription already subscribed!');
         assert.ok(!subscription.keyPropertyName || key !== StoreBase.Key_All,
@@ -249,6 +249,9 @@ abstract class ComponentBase<P extends React.Props<any>, S extends Object> exten
                 'Subscription created with specific key of all');
 
         if (key) {
+            if (_.isNumber(key)) {
+                key = key.toString();
+            }
             subscription._subscriptionToken = subscription.store.subscribe(subscription._lambda, key);
             subscription._subscriptionKey = key;
         } else {

src/Types.ts
@@ -19,7 +19,7 @@ export interface StoreSubscription<S> {
     // against (and detecting changes to)
     keyPropertyName?: string;
     // To subscribe to a specific key instead of the contents of a property, use this
-    specificKeyValue?: string;
+    specificKeyValue?: string|number;
     // Allow toggling of subscription based on prop
     enablePropertyName?: string;
 }

commit 3e84694898904fe5c63998022c2344fb0bc2a581

Fix a regression introduced in 3999239d3c0 that broke keyless triggers

src/StoreBase.ts
@@ -73,7 +73,10 @@ export abstract class StoreBase {
     // If you trigger a specific set of keys, then it will only trigger that specific set of callbacks (and subscriptions marked
     // as "All" keyed).  If the key is all, it will trigger all callbacks.
     protected trigger(keyOrKeys?: string|number|(string|number)[]) {
-        const keys = _.map(_.isArray(keyOrKeys) ? keyOrKeys : [keyOrKeys], (key, i) => _.isNumber(key) ? key.toString() : key);
+        let keys: string[];
+        if (keyOrKeys) {
+            keys = _.map(_.isArray(keyOrKeys) ? keyOrKeys : [keyOrKeys], (key, i) => _.isNumber(key) ? key.toString() : key);
+        }
 
         // Build a list of callbacks to call, trying to accumulate keys into a single callback set to avoid multiple callbacks
         // to the same target with different keys.