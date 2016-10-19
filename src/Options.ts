63a7392b33d2fabccedaa95f0a2ca39422c95978
David de Regt <dadere@microsoft.com>
2016-10-19 13:56:55 -0700

    0.0.9: Fixing development defaults to make more sense (false most of the time unless you explicitly enable it with NODE_ENV)

--- src/Options.ts
+++ src/Options.ts
@@ -13,15 +13,21 @@ export interface IOptions {
     setTimeout: (callback: () => void, timeoutMs?: number) => number;
     clearTimeout: (id: number) => void;
 
-    // Enables development mode -- more run-time checks
+    // Enables development mode -- more run-time checks.  By default, matches the NODE_ENV environment variable -- only set to true when
+    // NODE_ENV is set and is set to something other than "production".
     development: boolean;
 };
+
+interface IProcess {
+    env: { NODE_ENV?: string };
+}
+declare var process: IProcess;
 
 let OptionsVals: IOptions = {
     setTimeout: setTimeout.bind(null),
     clearTimeout: clearTimeout.bind(null),
 
-    development: true
+    development: typeof process !== 'undefined' && process.env && process.env.NODE_ENV !== 'production'
 };
 
 export default OptionsVals;
