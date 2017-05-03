455091166ca13bd609e9a525d97db28cd609f6f5
Brent Erickson <berickson1@users.noreply.github.com>
2017-05-03 16:12:30 -0700

    Fix a bug with @autoSubscribeWithKey and add unit tests to go with it

--- src/AutoSubscriptions.ts
+++ src/AutoSubscriptions.ts
@@ -264,7 +264,7 @@ function makeAutoSubscribeDecorator<T extends Function>(shallow = false, default
 
 export var autoSubscribe = makeAutoSubscribeDecorator(true, [StoreBase.Key_All]);
 export function autoSubscribeWithKey(keyOrKeys: string|number|(string|number)[]) {
-    assert.ok(keyOrKeys, 'Must specify a key when using autoSubscribeWithKey');
+    assert.ok(keyOrKeys || _.isNumber(keyOrKeys), 'Must specify a key when using autoSubscribeWithKey');
     const keys = _.map(_.isArray(keyOrKeys) ? keyOrKeys : [keyOrKeys], key => _.isNumber(key) ? key.toString() : key);
     return makeAutoSubscribeDecorator(true, keys);
 };
