62ff5165172ee80cb54d678a39f4c1c85000ecb0
Alexader <alexander.v.tarasyuk@gmail.com>
2018-01-20 23:53:29 +0200

    Fix code style

--- karma/karma.config.js
+++ karma/karma.config.js
@@ -1,38 +1,38 @@
 const webpack = require('./webpack.test');
 
 module.exports = config => (
-  config.set({
-    basePath: '',
-    frameworks: ['jasmine'],
-    reporters: ['spec', 'kjhtml'],
+    config.set({
+        basePath: '',
+        frameworks: ['jasmine'],
+        reporters: ['spec', 'kjhtml'],
 
-    plugins: [
-      'karma-jasmine-html-reporter',
-      'karma-sourcemap-loader',
-      'karma-chrome-launcher',
-      'karma-spec-reporter',
-      'karma-jasmine',
-      'karma-webpack',
-    ],
+        plugins: [
+            'karma-jasmine-html-reporter',
+            'karma-sourcemap-loader',
+            'karma-chrome-launcher',
+            'karma-spec-reporter',
+            'karma-jasmine',
+            'karma-webpack',
+        ],
 
-    preprocessors: {
-      './karma/karma.entry.js': ['webpack', 'sourcemap'],
-    },
+        preprocessors: {
+            './karma/karma.entry.js': ['webpack', 'sourcemap'],
+        },
 
-    files: [
-      { pattern: './karma/karma.entry.js', watched: false },
-    ],
+        files: [
+            { pattern: './karma/karma.entry.js', watched: false },
+        ],
 
-    logLevel: config.LOG_INFO,
-    colors: true,
-    mime: {
-      'text/x-typescript': ['ts', 'tsx'],
-    },
+        logLevel: config.LOG_INFO,
+        colors: true,
+        mime: {
+            'text/x-typescript': ['ts', 'tsx'],
+        },
 
-    webpack,
+        webpack,
 
-    webpackMiddleware: {
-      stats: 'errors-only',
-    }
-  })
+        webpackMiddleware: {
+            stats: 'errors-only',
+        }
+    })
 );
