5777db07458291014b8167943caa82739e7280a1
Alexander T <alexander.v.tarasyuk@gmail.com>
2018-03-19 21:19:59 +0200

    use re-exports (#65)

--- src/ReSub.ts
+++ src/ReSub.ts
@@ -1,27 +1,21 @@
 /**
-* ReSub.ts
-* Author: David de Regt
-* Copyright: Microsoft 2016
-*
-* Shared basic types for ReSub.
-*/
+ * ReSub.ts
+ * Author: David de Regt
+ * Copyright: Microsoft 2016
+ *
+ * Shared basic types for ReSub.
+ */
 
-import ComponentBaseI = require('./ComponentBase');
-import AutoSubscriptionsI = require('./AutoSubscriptions');
-import StoreBaseI = require('./StoreBase');
-import TypesI = require('./Types');
-import OptionsI = require('./Options');
+import * as Types from './Types';
 
-export const ComponentBase = ComponentBaseI.ComponentBase;
-
-export const StoreBase = StoreBaseI.StoreBase;
-
-export const AutoSubscribeStore = AutoSubscriptionsI.AutoSubscribeStore;
-export const autoSubscribe = AutoSubscriptionsI.autoSubscribe;
-export const autoSubscribeWithKey = AutoSubscriptionsI.autoSubscribeWithKey;
-export const key = AutoSubscriptionsI.key;
-export const disableWarnings = AutoSubscriptionsI.disableWarnings;
-
-export const Options = OptionsI.default;
-
-export import Types = TypesI;
+export {
+    autoSubscribeWithKey,
+    AutoSubscribeStore,
+    disableWarnings,
+    autoSubscribe,
+    key,
+} from './AutoSubscriptions';
+export { default as Options } from './Options';
+export { ComponentBase } from './ComponentBase';
+export { StoreBase } from './StoreBase';
+export { Types };
