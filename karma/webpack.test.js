6224ff74743a834d7431a21cf3f46bdf9ed3f0f0
Alexander T <alexander.v.tarasyuk@gmail.com>
2018-03-20 19:27:48 +0200

    Update dependencies, Jasmine 2.* -> Jasmine 3. Webpack 3 -> Webpack 4 (#66)

--- karma/webpack.test.js
+++ karma/webpack.test.js
@@ -1,6 +1,12 @@
-const { SourceMapDevToolPlugin } = require('webpack');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const path = require('path');
+const ROOT_PATH = path.resolve(__dirname, '..');
+const CONFIG_PATH = path.resolve(ROOT_PATH, 'tsconfig.test.json');
 
 module.exports = {
+    devtool: 'inline-source-map',
+    mode: 'development',
+
     resolve: {
         extensions: ['.js', '.ts', 'tsx']
     },
@@ -8,15 +14,17 @@ module.exports = {
     module: {
         rules: [{
             test: /\.tsx?$/,
-            loader: 'awesome-typescript-loader',
+            loader: 'ts-loader',
+            options: {
+                transpileOnly: true,
+                configFile: CONFIG_PATH,
+                context: ROOT_PATH,
+            },
             exclude: /node_modules/
         }]
     },
 
     plugins: [
-        new SourceMapDevToolPlugin({
-            filename: null,
-            test: /\.(js|ts|tsx)($|\?)/i,
-        })
+        new ForkTsCheckerWebpackPlugin({ tsconfig: CONFIG_PATH }),
     ]
 };
