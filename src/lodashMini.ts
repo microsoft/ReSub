d6256812f63852f8fd8f8902720748ecfdbcbd1c
Alexander T <alexander.v.tarasyuk@gmail.com>
2019-01-09 19:53:26 +0200

    use spread operator to copy array (#92)

--- src/lodashMini.ts
+++ src/lodashMini.ts
@@ -19,7 +19,6 @@ import indexOf from 'lodash/indexOf';
 import extend from 'lodash/extend';
 import remove from 'lodash/remove';
 import values from 'lodash/values';
-import clone from 'lodash/clone';
 import union from 'lodash/union';
 import uniq from 'lodash/uniq';
 import pull from 'lodash/pull';
@@ -46,7 +45,6 @@ export {
     extend,
     remove,
     values,
-    clone,
     union,
     uniq,
     pull,
