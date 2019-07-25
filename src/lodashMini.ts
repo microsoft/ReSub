93ab77deffe38efa29601e387c1d0296a677e06b
Brent Erickson <berickson1@users.noreply.github.com>
2019-07-24 22:15:33 -0700

    Remove a bunch more lodash functions Remove isFunction, isString, isNumber, isEmpty, extend, get and replace with internal implementations

--- src/lodashMini.ts
+++ src/lodashMini.ts
@@ -7,23 +7,29 @@
 * Imports a subset of lodash library needed for ReSub
 */
 
-import isFunction from 'lodash/isFunction';
-import isString from 'lodash/isString';
-import isNumber from 'lodash/isNumber';
 import flatten from 'lodash/flatten';
-import isEmpty from 'lodash/isEmpty';
 import isEqual from 'lodash/isEqual';
 import forEach from 'lodash/forEach';
-import extend from 'lodash/extend';
 import remove from 'lodash/remove';
 import values from 'lodash/values';
 import uniq from 'lodash/uniq';
 import pull from 'lodash/pull';
 import find from 'lodash/find';
-import get from 'lodash/get';
 
 export interface Dictionary<T> {
     [index: string]: T;
+}
+
+function isFunction(object: any): object is Function {
+    return typeof object === 'function';
+}
+
+function isString(object: any): object is string {
+    return typeof object === 'string';
+}
+
+function isNumber(object: any): object is number {
+    return typeof object === 'number';
 }
 
 export {
@@ -31,14 +37,11 @@ export {
     isString,
     isNumber,
     flatten,
-    isEmpty,
     isEqual,
     forEach,
-    extend,
     remove,
     values,
     uniq,
     pull,
     find,
-    get,
 };
