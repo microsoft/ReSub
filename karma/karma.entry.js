292a30c5938983f5d39c280413785e3c2edf55c6
David de Regt <deregtd@outlook.com>
2023-09-24 21:21:30 -0400

    trying to update to latest versions

--- karma/karma.entry.js
+++ karma/karma.entry.js
@@ -1,5 +1,9 @@
 require('./enzyme.config');
 
+// Force development:true for unit tests
+var opts = require('../src/Options.ts');
+opts.default.development = true;
+
 const context = require
     .context('../test/', true, /\.spec\.tsx?$/);
 
